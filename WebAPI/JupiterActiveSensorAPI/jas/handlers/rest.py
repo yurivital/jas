22b38f33d85df16d1f4e80a134021451a1b0b4db
Yuri VITAL <vital.systm@gmail.com>
2015-02-25 19:39:16 +0100

    API : Documentation

--- WebAPI/JupiterActiveSensorAPI/jas/handlers/rest.py
+++ WebAPI/JupiterActiveSensorAPI/jas/handlers/rest.py
@@ -7,54 +7,63 @@ from jas.handlers.viewhandler import *
 from jas.models.temperaturerecord import *
 from jas.models.device import *
 
-## TEMP
+""" Handle Http request for temperature recording """
 class SetTemperatureRecordHandler(ViewHandler):
+    """ Return the documentation of the api """
     def get(self):
-	template = self.load_template('jasapidoc.html')
+        template = self.load_template('jasapidoc.html')
         self.response.write(template.render())
         
-
+    """ Persist in database the temperature records performed by the device """
     def post(self, device_id):
-	request_id = str(uuid.uuid1())
-	logging.info("SetRecord for device {0} - request {1}".format(device_id,request_id))
-
-	self.response.headers['Content-Type'] = "application/json"
+        request_id = str(uuid.uuid1())
+        logging.info("SetRecord for device {0} - request {1}".format(device_id,request_id))
+        self.response.headers['Content-Type'] = "application/json"
         device_ok = Device.is_active_registred_device(device_key, device_id)
         if (device_ok  == False):
-               msg_error = { "status" : "KO", 
-			       "message" : "The device is not registred and active", 
-			       "requestId" : request_id
-			       }
-	       logging.info(msg_error)
-	       return self.response.out.write(json.dumps(msg_error))
+            msg_error = { "status" : "KO",
+                          "message" : "The device is not registred and active",
+                          "requestId" : request_id
+                          }
+            logging.info(msg_error)
+            return self.response.out.write(json.dumps(msg_error))
+        try:
+            temperature_data = json.loads(self.request.body)
+        except ValueError:
+            msg_error = { "status" : "KO",
+                      "message" : "unable to convert JSON data",
+                      "requestId" : request_id
+                      }
+            logging.info(msg_error)
+            return self.response.out.write(json.dumps(msg_error));
+        except:
+            msg_error = { "status" : "KO", 
+                "message" : "unexpected error",
+                "requestId" : request_id
+                }
+            logging.info(msg_error)
+            return self.response.out.write(json.dumps(msg_error));
+        device = Device.query_device_id(device_key,device_id )
+        nb_records = 0
+        status = "OK"
+        message = ""
+        for temperature in temperature_data :
+            try:
+                tempData = TemperatureRecord()
+                tempData.device_id = device_id
+                tempData.sensor_id= temperature['sensorId']
+                tempData.temperature = temperature['temperature']
+                device.temperatures.append(tempData)
+                device.put();
+                nb_records = nb_records + 1
+            except:
+                message = "An unexpected error occured while persisting data : {0}".format(temperature)
+                status = "WARN"
 
-     	try:
-        	bodyData = json.loads(self.request.body)
-	except ValueError:
-		msg_error = { "status" : "KO",
-				"message" : "unable to convert JSON data",
-				"requestId" : request_id
-				}
-		logging.info(msg_error)
-		return self.response.out.write(json.dumps(msg_error));
-	except:
-		msg_error = { "status" : "KO", 
-				"message" : "unexpected error",
-				"requestId" : request_id
-				}
-		logging.info(msg_error)
-		return self.response.out.write(json.dumps(msg_error));
-
-	nb_records = len(bodyData)
-        tempData = TemperatureRecord(parent=tempdata_key)
-        tempData.device_id = device_id
-        tempData.sensor_id= self.request.get('sensorID')
-        tempData.temprature = self.request.get('temperature')
-        tempData.put()
-	msg_ok = { "status" : "OK", 
-			"processed" : nb_records,
-			"requestId" : request_id}
-	self.response.out.write(json.dumps(msg_ok));
-
-   
-    
+        msg_ok = { "status" : status,
+                   "processed" : nb_records,
+                   "requestId" : request_id,
+                   "message" : message
+                   }
+        logging.info(msg_ok)
+        self.response.out.write(json.dumps(msg_ok));
--- WebAPI/JupiterActiveSensorAPI/jas/models/temperaturerecord.py
+++ WebAPI/JupiterActiveSensorAPI/jas/models/temperaturerecord.py
@@ -2,12 +2,14 @@ import logging
 import datetime
 from google.appengine.ext import ndb
 
-
+""" ancestor key for consistant temperature record """
 tempdata_key = ndb.Key('TempData', 'default_tempdata')
 
+""" Define a persisted temperature record """
 class TemperatureRecord(ndb.Model):
+	""" Sensor identity number """
 	sensor_id	= ndb.StringProperty(indexed=True)
+	""" Value ot the measured temperature """
 	temperature	= ndb.FloatProperty()
+	""" Value of the date and time of the measure """
 	timestamp	= ndb.DateTimeProperty(auto_now_add=True, indexed=True)
-
-
