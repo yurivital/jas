2ac4c6f2812b27bdd16b2ef9b6a1b05103aef2da
Yuri VITAL <vital.systm@gmail.com>
2015-03-30 19:20:09 +0200

    Fix : bundles are not relative URL

--- Capture/RestApiClient/RestApiClient/RestApiClient.ino
+++ Capture/RestApiClient/RestApiClient/RestApiClient.ino
@@ -1,13 +1,15 @@
 #include <SPI.h>
 #include <Ethernet.h>
-
+#include <String.h>
+#include <SD.h>
 // Enter a MAC address for your controller below.
 // Newer Ethernet shields have a MAC address printed on a sticker on the shield
 byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
 
 // REST Server
 char server[] = "www.google.com";
-
+String action = "/API/SetTemperatureRecord/TEST";
+String payLoad = "[{ 'sensorID' : 'SENSOR01', 'temperature': 12.3, 'timestamp' : '' } ]";
 // Set the static IP address to use if the DHCP fails to assign
 IPAddress ip(192,168,1,42);
 
@@ -27,17 +29,24 @@ void setup(){
   }
   // give the Ethernet shield a second to initialize:
   delay(1000);
-  
 }
+ 
 
 void loop(){
   
    if (client.connect(server, 80)) {
     Serial.println("connected");
     // Make a HTTP request:
-    client.println("GET /search?q=arduino HTTP/1.1");
-    client.println("Host: www.google.com");
+    client.println("POST " + action  + " HTTP/1.1");
+    client.println("Accept-Encoding: text,plain");
+    client.println("Content-Type: application/json");
+    client.println("Content-Length: " + payLoad.length());
     client.println("Connection: close");
+    client.println("User-Agent: Arduino-HttpJasClient/1.0");
+    client.println("Host: www.google.com");
+    client.println("");
+    client.println(payLoad); 
     client.println();
+    
   } 
 }
