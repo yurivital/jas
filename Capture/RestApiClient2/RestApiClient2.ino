dd14648a6b32efef89a76581e95de9ccef7e46c3
Yuri VITAL <vital.systm@gmail.com>
2015-04-11 11:50:37 +0200

    Fix : Capture  HTTP header

--- Capture/RestApiClient2/RestApiClient2.ino
+++ Capture/RestApiClient2/RestApiClient2.ino
@@ -20,13 +20,15 @@
 #define W5200_CS  10
 #define SDCARD_CS 4
 
+char newline = 10; 
 
 // Enter a MAC address for your controller below.
 // Newer Ethernet shields have a MAC address printed on a sticker on the shield
-byte mac[] = {  0x00, 0xAA, 0xBB, 0xCC, 0xDE, 0x02 };
+byte mac[] = {  
+  0x00, 0xAA, 0xBB, 0xCC, 0xDE, 0x02 };
 
 char serverName[] = "jupiteractivesensor.appspot.com";
-String action = "/API/SetTemperatureRecord/TEST";
+String action = "/API/SetTemperatureRecord/TEST01";
 String payLoad = "[{ 'sensorID' : 'SENSOR01', 'temperature': 12.3, 'timestamp' : '' } ]";
 boolean ethernetReady = false;
 // Initialize the Ethernet client library
@@ -35,61 +37,66 @@ boolean ethernetReady = false;
 EthernetClient client;
 
 void setup() {
- // Open serial communications and wait for port to open:
+  // Open serial communications and wait for port to open:
   Serial.begin(115200);
-  pinMode(SDCARD_CS,OUTPUT);
- digitalWrite(SDCARD_CS,HIGH);//Deselect the SD card
   Serial.println("Setup");
+
+  pinMode(SDCARD_CS,OUTPUT);
+  digitalWrite(SDCARD_CS,HIGH);//Deselect the SD card
+
   // start the Ethernet connection:
   if (Ethernet.begin(mac) == 0) {
     Serial.println("Failed to configure Ethernet using DHCP");
     // no point in carrying on, so do nothing forevermore:
     return;
   }
+  Serial.println("Configuring Ethernet with the MAC adresse 00:aa:bb:cc:de:02");
   // give the Ethernet shield a second to initialize:
   delay(1000);
   ethernetReady = true;
-  Serial.println("connecting...");
-
-  // if you get a connection, report back via serial:
-  
+  Serial.print("IP Adresss : ")
+    Serial.println(Ethernet.localIP());
 }
 
+// Send the HttpResquest to the JAS Web Api server
 void postRequest()
 {
-    client.println("POST " + action  + " HTTP/1.1");
-    client.println("Accept: text/plain");
-    client.println("Content-Type: application/json");
-    client.println("Content-Length: " + payLoad.length());
-    client.println("Connection: close");
-    client.println("User-Agent: Arduino-HttpJasClient/1.0");
-    client.print("Host: ");
-    client.println(serverName);
-    client.println("");
-    client.println(payLoad); 
-    client.println();
+  String req = "POST " + action  + " HTTP/1.1" + newline;
+  req += "Accept: text/plain" + newline;
+  req += "Content-Type: application/json" + newline;
+  req += "Content-Length: " + String(payLoad.length()) + newline;
+  req += "User-Agent: Arduino-JasClient/1.0" + newline;
+  req += "Host: " + String(serverName) + newline + newline;
+  req += payLoad;
+
+  Serial.println("Sending request :");
+  Serial.println(req);
+  client.println(req);
+  client.println();
 }
 
 void loop()
 {
-  
+  Serial.println("connecting...");
   if ( ethernetReady 
-       && client.connect(serverName, 80)) {
+    && client.connect(serverName, 80)) {
     Serial.println("connected");
     // Make a HTTP request:
     postRequest();
   } 
   else {
-    // kf you didn't get a connection to the server:
-    Serial.println("connection failed");
+    // if you didn't get a connection to the server:
+    Serial.println("connection failed or ethernet nor read");
     return;
   }
-  
+
   // if there are incoming bytes available 
   // from the server, read them and print them:
   if (client.available()) {
-    char c = client.read();
-    Serial.print(c);
+    Serial.println("Response :");
+    while(char c = client.read() != NULL){
+      Serial.print(c);
+    }
   }
+  client.stop();
 }
-
